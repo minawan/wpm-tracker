ca3c9452ae1cba961a1ffd57fdf390e69da2feb6
Minaki Seirawan <Seirawan.Minaki@gmail.com>
2019-06-22 21:22:54 -0700

    Compute running max using trigger

--- stat_validator.py
+++ stat_validator.py
@@ -30,13 +30,25 @@ FIELD_NAMES = (ROWID, DATE, WPM, HIGH)
 SQL_CREATE_TABLE = '''CREATE TABLE IF NOT EXISTS {table} (
     {date} date NOT NULL CHECK ({date} IS strftime('%Y-%m-%d', {date})),
     {wpm} integer NOT NULL CHECK ({wpm} > 0),
-    {high} integer NOT NULL CHECK ({high} > 0),
+    {high} integer CHECK ({high} > 0),
     CONSTRAINT check_running_max CHECK ({high} >= {wpm})
 );'''.format(table=TABLE_NAME, date=DATE, wpm=WPM, high=HIGH)
+SQL_CREATE_TRIGGER = '''CREATE TRIGGER IF NOT EXISTS running_max
+    AFTER INSERT ON {table} FOR EACH ROW
+    BEGIN
+        UPDATE {table}
+        SET {high} = NEW.{wpm}
+        WHERE {rowid} = NEW.{rowid}
+          AND {rowid} = 1;
+        UPDATE {table}
+        SET {high} = max(NEW.{wpm}, (SELECT high FROM {table} WHERE {rowid} = NEW.{rowid} - 1))
+        WHERE {rowid} = NEW.{rowid}
+          AND {rowid} > 1;
+    END;'''.format(table=TABLE_NAME, rowid=ROWID, high=HIGH, wpm=WPM)
 SQL_QUERY_TABLE_BY_ROWID = 'SELECT {rowid}, * FROM {table} WHERE {rowid}=?'.format(
     table=TABLE_NAME, rowid=ROWID)
-SQL_INSERT = '''INSERT INTO {table}({date}, {wpm}, {high})
-    VALUES(?, ?, ?)'''.format(table=TABLE_NAME, date=DATE, wpm=WPM, high=HIGH)
+SQL_INSERT = '''INSERT INTO {table}({date}, {wpm})
+    VALUES(?, ?)'''.format(table=TABLE_NAME, date=DATE, wpm=WPM)
 
 StatRecord = namedtuple('StatRecord', ' '.join(FIELD_NAMES))
 
@@ -47,14 +59,20 @@ def get_validator():
     validator.add_record_length_check('EX2', 'unexpected record length')
     return validator
 
-def get_record(stat_db, rowid):
+def get_record_from_image(img_file):
+    date = re.split(r'\.|_', img_file)[0]
+    img_content = pytesseract.image_to_string(cv2.imread(img_file))
+    wpm = int(re.search(r'(\d+) *WPM', img_content).group(1))
+    return date, wpm
+
+def get_record_from_db(stat_db, rowid):
     cur = stat_db.cursor()
     cur.execute(SQL_QUERY_TABLE_BY_ROWID, (rowid,))
     return cur.fetchone()
 
 def check_record_db(stat_db, row):
     actual = StatRecord(int(row[ROWID]), row[DATE], int(row[WPM]), int(row[HIGH]))
-    db_row = get_record(stat_db, getattr(actual, ROWID))
+    db_row = get_record_from_db(stat_db, getattr(actual, ROWID))
     if not db_row:
         raise RecordError('EX3', 'Row not found among the generated records.')
     expected = StatRecord(*db_row)
@@ -67,16 +85,11 @@ def main(_):
     with open(FLAGS.stat_file, 'r') as input_csv_file, \
          sqlite3.connect(FLAGS.db_file) as stat_db:
         stat_db.cursor().execute(SQL_CREATE_TABLE)
-        high = 0
+        stat_db.cursor().execute(SQL_CREATE_TRIGGER)
         for rowid, img_file in enumerate(sorted(glob.glob('*.png')), 1):
-            if get_record(stat_db, rowid):
+            if get_record_from_db(stat_db, rowid):
                 continue
-            date = re.split(r'\.|_', img_file)[0]
-            img = cv2.imread(img_file)
-            img_content = pytesseract.image_to_string(img)
-            wpm = int(re.search(r'(\d+) *WPM', img_content).group(1))
-            high = max(high, wpm)
-            stat_db.cursor().execute(SQL_INSERT, (date, wpm, high))
+            stat_db.cursor().execute(SQL_INSERT, get_record_from_image(img_file))
         validator.add_record_check(partial(check_record_db, stat_db))
         data = csv.reader(input_csv_file, delimiter=',')
         problems = validator.validate(data)
