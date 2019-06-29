ad9bb990b90404747c42d5d7d5bf7abebf71ec78
Minaki Seirawan <Seirawan.Minaki@gmail.com>
2019-06-29 16:32:01 -0700

    Parse date into Day

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,25 +3,34 @@ module Lib
     ) where
 
 import Data.List (intercalate)
+import Data.Time.Calendar (Day)
+import Data.Time.Format (defaultTimeLocale, parseTimeM)
 import Database.HDBC.Sqlite3 (connectSqlite3)
 import Database.HDBC
 
 type RowId = Integer
-type Date = String
+type Date = Day
 type Wpm = Integer
 type High = Integer
 
-data StatEntry = StatEntry RowId Date Wpm High
+data StatEntry = StatEntry { rowIdField :: RowId
+                           , dateField :: Date
+                           , wpmField :: Wpm
+                           , highField :: High
+                           }
 
 instance Show StatEntry where
-  show (StatEntry rowId date wpm high) = intercalate "," [show rowId, date, show wpm, show high]
+  show (StatEntry rowId date wpm high) = intercalate "," $ [show rowId, show date, show wpm, show high]
 
 convRow :: [SqlValue] -> Maybe StatEntry
-convRow [sqlRowId, sqlDate, sqlWpm, sqlHigh] = Just $ StatEntry rowId date wpm high
-  where rowId = (fromSql sqlRowId)::Integer
-        date = (fromSql sqlDate)::String
-        wpm = (fromSql sqlWpm)::Integer
-        high = (fromSql sqlHigh)::Integer
+convRow [sqlRowId, sqlDate, sqlWpm, sqlHigh] =
+    case dateM of
+      Just date -> Just $ StatEntry rowId date wpm high
+      Nothing -> Nothing
+  where rowId = fromSql sqlRowId
+        dateM = parseTimeM True defaultTimeLocale "%Y-%m-%d" $ fromSql sqlDate
+        wpm = fromSql sqlWpm
+        high = fromSql sqlHigh
 convRow _ = Nothing
 
 readAllWpm :: String -> String -> IO ()
@@ -29,7 +38,8 @@ readAllWpm _ dbFilename = do
   conn <- connectSqlite3 dbFilename
   r <- quickQuery' conn
        "SELECT oid, * from stat ORDER BY oid" []
-  case sequence $ map convRow r of
-    Just rows -> mapM_ (putStrLn . show) rows
-    Nothing -> return ()
+  let rows = case sequence $ map convRow r of
+               Just rows -> rows
+               Nothing -> []
+  mapM_ (putStrLn . show) rows
   disconnect conn
