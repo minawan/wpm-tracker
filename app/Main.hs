4aa8373932cb9f45ebeeca4332f14be0c612a56e
Minaki Seirawan <Seirawan.Minaki@gmail.com>
2019-06-29 16:57:59 -0700

    Move IO actions from Lib to Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,24 @@
 module Main where
 
+import Database.HDBC.Sqlite3 (connectSqlite3)
+import Database.HDBC
 import System.Environment
+
 import Lib
+
+selectRowsFromStatTable :: String
+selectRowsFromStatTable = "SELECT oid, * from stat ORDER BY oid"
 
 main :: IO ()
 main = do
   args <- getArgs
-  readAllWpm (args !! 0) (args !! 1)
+  let csvFilename = args !! 0
+  let dbFilename = args !! 1
+  conn <- connectSqlite3 dbFilename
+  queryResult <- quickQuery' conn selectRowsFromStatTable []
+
+  let rows = case sequence $ readAllRows queryResult of
+               Just rows -> rows
+               Nothing -> []
+  mapM_ (putStrLn . show) rows
+  disconnect conn
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,12 @@
 module Lib
-    ( readAllWpm
+    ( StatEntry
+    , readAllRows
     ) where
 
 import Data.List (intercalate)
 import Data.Time.Calendar (Day)
 import Data.Time.Format (defaultTimeLocale, parseTimeM)
-import Database.HDBC.Sqlite3 (connectSqlite3)
-import Database.HDBC
+import Database.HDBC (SqlValue, fromSql)
 
 type RowId = Integer
 type Date = Day
@@ -33,13 +33,5 @@ convRow [sqlRowId, sqlDate, sqlWpm, sqlHigh] =
         high = fromSql sqlHigh
 convRow _ = Nothing
 
-readAllWpm :: String -> String -> IO ()
-readAllWpm _ dbFilename = do
-  conn <- connectSqlite3 dbFilename
-  r <- quickQuery' conn
-       "SELECT oid, * from stat ORDER BY oid" []
-  let rows = case sequence $ map convRow r of
-               Just rows -> rows
-               Nothing -> []
-  mapM_ (putStrLn . show) rows
-  disconnect conn
+readAllRows :: [[SqlValue]] -> [Maybe StatEntry]
+readAllRows = map convRow
