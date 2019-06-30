9578c389116ee767afdd82f6dc88f0a99ac3bef9
Minaki Seirawan <Seirawan.Minaki@gmail.com>
2019-06-29 20:20:58 -0700

    Validate rows against their previous rows

--- src/Lib.hs
+++ src/Lib.hs
@@ -43,6 +43,35 @@ checkHighGeWpm (StatEntry rowId _ wpm high)
   | wpm <= high = []
   | otherwise = [printf "Row %d: high=%d < wpm=%d" rowId high wpm]
 
+checkSequentialRowId :: StatEntry -> StatEntry -> [String]
+checkSequentialRowId (StatEntry rowId1 _ _ _) (StatEntry rowId2 _ _ _)
+  | rowId1 == rowId2 - 1 = []
+  | otherwise = [msg]
+  where msg = printf "Non-sequential row IDs %d and %d" rowId1 rowId2
+
+checkDatesInOrder :: StatEntry -> StatEntry -> [String]
+checkDatesInOrder (StatEntry rowId1 date1 _ _) (StatEntry rowId2 date2 _ _)
+  | date1 <= date2 = []
+  | otherwise = [msg]
+  where msg = printf "Dates not in order: row%d.date=%s > row%d.date=%s"
+                     rowId1 (show date1) rowId2 (show date2)
+
+checkRunningMax :: StatEntry -> StatEntry -> [String]
+checkRunningMax (StatEntry rowId1 _ _ high1) (StatEntry rowId2 _ wpm2 high2)
+  | high2 == runningMax = []
+  | otherwise = [msg]
+  where runningMax = max high1 wpm2
+        msg = printf "Invalid running max: row%d.high=%d != " rowId2 high2
+           ++ printf "max row%d.high=%d " rowId1 high1
+           ++ printf "row%d.wpm=%d == %d" rowId2 wpm2 runningMax
+
+
+checkContiguousEntries :: StatEntry -> StatEntry -> [String]
+checkContiguousEntries entry1 entry2 = checkSequentialRowId entry1 entry2
+                                    ++ checkDatesInOrder entry1 entry2
+                                    ++ checkRunningMax entry1 entry2
+
 validateStatEntries :: [StatEntry] -> [String]
 validateStatEntries [] = []
-validateStatEntries (hd:tl) = checkHighGeWpm hd ++ validateStatEntries tl
+validateStatEntries [entry] = checkHighGeWpm entry
+validateStatEntries (entry1:entry2:tl) = checkHighGeWpm entry1 ++ checkContiguousEntries entry1 entry2 ++ validateStatEntries (entry2:tl)
